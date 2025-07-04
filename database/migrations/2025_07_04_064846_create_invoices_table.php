65a3a912969c4dde0899a73f31a62fe0e75b5175
Monisha <YOUR_EMAIL>
2025-07-04 12:37:50 +0530

    invoice table migration file created

---
+++ database/migrations/2025_07_04_064846_create_invoices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name');
+            $table->string('email');
+            $table->text('item_description');
+            $table->integer('quantity');
+            $table->decimal('price_per_item');
+            $table->decimal('tax_percentage');
+            $table->decimal('subtotal');
+            $table->decimal('taxAmount');
+            $table->decimal('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice');
+    }
+};
